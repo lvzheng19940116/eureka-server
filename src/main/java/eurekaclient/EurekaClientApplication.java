ca427b9a63ff2afa95b11b4c4ae221c32413fc1e
a645661167 <a645661167@10.116.58.34>
2018-08-20 10:38:41 +0800

    初始化

---
+++ src/main/java/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,16 @@
+package eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
